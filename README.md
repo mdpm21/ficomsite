cd34505bdf8e3aa11dbbed576cf2f1cbf8740fe2
mdpm21 <daffapm.21@gmail.com>
2023-06-09 22:57:52 +0800

    final version

---
+++ README.md
@@ -0,0 +1,65 @@
+# Ficomsite (Fishing Recommendation Website)
+
+This project aims to assist fishermen in reducing losses by recommending the most profitable fish in a certain region. 
+
+Team Member:
+* (ML)  M172DKY4191 - Ya'sin Shofiani
+* (ML) M038DKY4294 - Ina Tantri Nareswari
+* (ML) M349DSX1778 - Rifki Rahman Nugraha
+* (CC) C261DKX4379 - Satria Bagus Eka Candra `Ketua`
+* (CC) C261DKX4147 - Muhammad Daffa Putra Mahardika
+* (CC) C261DKX4369 - Wahyu Harry Saputra Sembiring 
+
+## Technologies
+* Flask
+* Pandas
+* NumPy
+* Scikit-learn
+* Jinja
+* HTML
+* Bootstrap
+
+## Setup
+Follow these steps to run this project:
+
+1. Clone this repository to your local machine. Use the following command in your terminal:
+```
+git clone https://github.com/mdpm21/ficomsite.git
+```
+
+2. 	Install the required Python dependencies. In your terminal, navigate to the project directory and run:
+```
+pip install -r requirements.txt
+```
+&nbsp;Note: This step assumes you have Python and pip installed. If not, you may need to download and install them first.
+
+3. 	Run the Flask application:
+```
+python main.py
+```
+&nbsp;Note: After running this command, Flask will provide a local URL that you can use to view the web application in your browser.
+
+## Usage
+1. Open the web application in your browser using the URL provided by Flask.
+2. The homepage will show a list of recommended fish based on the volume of production and consumption.
+3. The recommendation is based on the KMeans clustering method.
+
+## Future work
+- Add more features that could influence the recommendation.
+- Improve the UI/UX of the web application.
+- Make the application responsive.
+
+## Credits
+This project was created by _PANEL_.
+
+## Result
+Base URL of Fiscomsite: https://panel-dot-panel-2023.et.r.appspot.com/
+
+![1](https://github.com/mdpm21/ficomsite/blob/main/static/result/1.png)
+![2](https://github.com/mdpm21/ficomsite/blob/main/static/result/2.png)
+![3](https://github.com/mdpm21/ficomsite/blob/main/static/result/3.png)
+![4](https://github.com/mdpm21/ficomsite/blob/main/static/result/4.png)
+![5](https://github.com/mdpm21/ficomsite/blob/main/static/result/5.png)
+![6](https://github.com/mdpm21/ficomsite/blob/main/static/result/6.png)
+![7](https://github.com/mdpm21/ficomsite/blob/main/static/result/7.png)
+
---
+++ main.py
@@ -0,0 +1,185 @@
+from flask import Flask, render_template, request
+import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from jinja2 import Environment, FileSystemLoader
+from sklearn.cluster import DBSCAN, KMeans
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('index.html')
+
+@app.route('/result1', methods=['GET', 'POST'])
+def index():
+    return tampil_halaman(1)
+
+@app.route('/halaman/<int:halaman>')
+def tampil_halaman(halaman):
+    JUMLAH_PER_HALAMAN = 15
+
+    # Load Data
+    data_cluster = pd.read_csv("https://storage.googleapis.com/tim_panel1/DataCobaNew.csv")
+    data_cluster = data_cluster[['Volume_Produksi','Konsumsi']]
+    
+    # Feature Scaling
+    sc_data_cluster = StandardScaler()
+    data_cluster_std = sc_data_cluster.fit_transform(data_cluster.astype(float))
+    
+    # Clustering with KMeans
+    kmeans = KMeans(n_clusters=5, random_state=42).fit(data_cluster_std)
+    labels = kmeans.labels_
+    
+
+    # Add labels to a new column
+    ikan = pd.read_csv("https://storage.googleapis.com/tim_panel1/DataCobaNew.csv")
+    ikan['Cluster'] = labels
+    
+    # Data Frame Manipulation
+    condition = [
+        (ikan['Cluster'] == 0),
+        (ikan['Cluster'] == 1),
+        (ikan['Cluster'] == 2),
+        (ikan['Cluster'] == 3),
+        (ikan['Cluster'] == 4)
+    ]
+    values = [
+        'Kurang Profit', 
+        'Normal',
+        'Cukup Profit',
+        'Sangat Profit',
+        'Kurang Profit'
+    ]
+    ikan['Status'] = np.select(condition, values)
+    
+    # Filter by search query
+    search_query = request.args.get('search')
+    kota_query = request.args.get('kota')
+    
+    if search_query and kota_query:
+        outputkab = ikan[
+        (ikan['Jenis_Ikan'].str.contains(search_query, case=False, na=False) | ikan['KabKota'].str.contains(search_query, case=False)) &
+        ikan['KabKota'].str.contains(kota_query, case=False, na=False)
+    ] 
+
+    if search_query:
+        outputkab = ikan[
+            (ikan['Jenis_Ikan'].str.contains(search_query, case=False, na=False)) &
+            (ikan['KabKota'].str.contains(kota_query, case=False, na=False))
+        ]
+    elif kota_query:
+        outputkab = ikan[ikan['KabKota'].str.contains(kota_query, case=False, na=False)]
+    else:
+        outputkab = ikan.copy()
+
+    # Take three columns
+    # outputkab = outputkab[['Jenis_Ikan', 'Status', 'KabKota']].copy()
+    
+    # Apply pagination
+    awal = (halaman-1) * JUMLAH_PER_HALAMAN
+    akhir = awal + JUMLAH_PER_HALAMAN
+    outputkab = outputkab[awal:akhir]
+
+    return render_template(
+        'index1.html', 
+        table=outputkab.to_dict(orient='records'), 
+        page=halaman, 
+        JUMLAH_PER_HALAMAN=JUMLAH_PER_HALAMAN,
+        search_query=search_query, kota_query=kota_query)
+
+
+
+
+@app.route('/result', methods=['GET', 'POST'])
+def index2():
+    return tampil_halaman2(1)
+
+
+
+@app.route('/halaman2/<int:halaman2>', methods=['GET', 'POST'])
+def tampil_halaman2(halaman2):
+    JUMLAH_PER_HALAMAN = 16
+    # Melakukan proses clustering
+    df = pd.read_csv("https://storage.googleapis.com/panel-buck/DataCobaNew.csv")
+    df = df[['Volume_Produksi', 'Konsumsi']]
+    data = StandardScaler().fit_transform(df.astype(float))
+
+    # Clustering menggunakan DBSCAN
+    db = DBSCAN(eps=0.18, min_samples=2).fit(data)
+    labels_dbscan = db.labels_
+
+    # Clustering menggunakan K-means
+    kmeans = KMeans(n_clusters=5, random_state=0)
+    labels_kmeans = kmeans.fit_predict(data)
+
+    # Menambahkan kolom label ke DataFrame
+    df['Cluster_DBSCAN'] = labels_dbscan
+    df['Cluster_KMeans'] = labels_kmeans
+
+    # Menyimpan hasil clustering
+    labels = labels_kmeans
+    # Load Data
+    ikan = pd.read_csv("https://storage.googleapis.com/panel-buck/DataCobaNew.csv")
+    ikan['Cluster'] = labels
+
+    # Data Frame Manipulation
+    condition = [
+        (ikan['Cluster'] == 0),
+        (ikan['Cluster'] == 1),
+        (ikan['Cluster'] == 2),
+        (ikan['Cluster'] == 3),
+        (ikan['Cluster'] == 4)
+    ]
+    values = ['Sehat untuk ditangkap', 
+          'Lebih baik dibatasi',
+          'Overfishing',
+          'Boleh dilebihkan',
+          'Aman']
+    ikan['Status'] = np.select(condition, values)
+
+    #sort custom
+    urutan = [
+        (ikan['Status'] == 'Sehat untuk ditangkap'),
+        (ikan['Status'] == 'Lebih baik dibatasi'),
+        (ikan['Status'] == 'Overfishing'),
+        (ikan['Status'] == 'Boleh dilebihkan'),
+        (ikan['Status'] == 'Aman'),
+    ]
+
+    values = [1, 2, 3, 4, 5]
+    ikan['temp'] = np.select(urutan, values)
+    ikan = ikan.sort_values(by=['temp'])
+
+    # Filter by search query
+    search_query = request.args.get('search')
+    kota_query = request.args.get('kota')
+    
+    if search_query:
+        outputkab2 = ikan[
+            ikan['Jenis_Ikan'].str.contains(search_query, case=False) |
+            ikan['KabKota'].str.contains(search_query, case=False) |
+            ikan['Status'].str.contains(search_query, case=False)
+        ]
+    elif kota_query:
+        outputkab2 = ikan[ikan['KabKota'].str.contains(kota_query, case=False)]
+    else:
+        outputkab2 = ikan.copy()
+
+        outputkab2 = outputkab2[['KabKota', 'Jenis_Ikan', 'Status']].copy()   
+
+    awal = (halaman2-1) * JUMLAH_PER_HALAMAN
+    akhir = awal + JUMLAH_PER_HALAMAN
+    outputkab2 = outputkab2[awal:akhir]
+
+    return render_template(
+        'index2.html', 
+        table=outputkab2.to_dict(orient='records'), 
+        page=halaman2, 
+        JUMLAH_PER_HALAMAN=JUMLAH_PER_HALAMAN,
+        search_query=search_query,kota_query=kota_query)
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
